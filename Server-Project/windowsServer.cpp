3db2e008bc89930b8b44cb857da65061ae4a688c
Selinaehu <99375272+Selinaehu@users.noreply.github.com>
2022-03-10 01:31:16 -0800

    All RPCs functional
    Added functionality for all RPCs. Sends output line-by-line to client for functions that print bingo boards. Included error processing with explanation to client for invalid input on all RPCs.

--- Server-Project/windowsServer.cpp
+++ Server-Project/windowsServer.cpp
@@ -12,6 +12,7 @@
 #include "bingogame.h"
 
 char* processParameter(char* buf) {
+    char* token;
     printf("input: %s\n", buf);
     char *start = buf;
     char *end = buf;
@@ -22,15 +23,14 @@ char* processParameter(char* buf) {
             end = buf;
         if(start < end && *start) {
             *end = 0;
-            char *token = start + 1;
+            token = start + 1;
             printf("token: %s\n", token);
-
             start = buf = end;
-            return token;
+            break;
         }
         buf++;
     }
-
+    return token;
 }
 
 // our thread for receiving commands
@@ -69,7 +69,8 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
             if(strstr(buf,"connect;USER1;PASSWORD1") ||
                strstr(buf,"connect;USER2;PASSWORD2") ||
                strstr(buf,"connect;USER3;PASSWORD3") ||
-               strstr(buf,"connect;USER4;PASSWORD4"))
+               strstr(buf,"connect;USER4;PASSWORD4") ||
+               strstr(buf, "c")) // last combination is for quick testing
             { // greet this user
                 printf("\nSuccessful login from user!\n");
 
@@ -83,15 +84,7 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
                 send(current_client,sendData,sizeof(sendData),0);
             }
         } else {
-            if(strstr(buf,"hello"))
-            { // greet this user
-                printf("\nreceived hello cmd");
-
-                strcpy(sendData,"hello from server\n");
-                Sleep(10);
-                send(current_client,sendData,sizeof(sendData),0);
-            }
-            else if(strstr(buf,"disconnect"))
+            if(strstr(buf,"disconnect"))
             { // disconnect this user
                 printf("\nReceived disconnect RPC\n");
 
@@ -104,12 +97,115 @@ DWORD WINAPI receive_cmds(LPVOID lpParam)
                 ExitThread(0);
             }
             else if(strstr(buf,"setMaxNum"))
-            { // disconnect this user
+            { // call setMaxNum function
                 printf("\nReceived setMaxNum RPC\n");
                 char* parameter = processParameter(buf);
-                bingoBoard.setMaxNum((string)parameter);
+                bool success = bingoBoard.setMaxNum((string)parameter);
+                char message [100];
+                if (success) {
+                    strcpy(message, (char*)"Successfully set! The max number that the server will call will be ");
+                    strcat(message, parameter);
+                } else {
+                    strcpy(message, (char*)"The max number provided was invalid. Please enter a new number greater than 25.");
+                }
+                strcpy(sendData, message);
+                Sleep(10);
+                send(current_client,sendData,sizeof(sendData),0);
+            }
+            else if(strstr(buf,"setTime"))
+            { // call setTime function
+                printf("\nReceived setTime RPC\n");
+                char* parameter = processParameter(buf);
+                bool success = bingoBoard.setTime((string)parameter);
+                char message [100];
+                if (success) {
+                    strcpy(message, (char*)"Successfully set! The delay between each server call will be ");
+                    strcat(message, parameter);
+                    strcat(message, (char*)" seconds.");
+                } else {
+                    strcpy(message, (char*)
+                            "The delay provided was invalid. Please enter a new number less than or"
+                            " equal to 10 seconds.");
+                }
+                strcpy(sendData, message);
+                Sleep(10);
+                send(current_client,sendData,sizeof(sendData),0);
+            }
+            else if(strstr(buf,"setBoard"))
+            { // call setBoard function
+                printf("\nReceived setBoard RPC\n");
+                char* parameter = processParameter(buf);
+                bool success = bingoBoard.setBoard((string)parameter);
+                char message [100];
+                if (success) {
+                    strcpy(message, (char*)"Successfully set bingo board!");
+                    strcpy(sendData, message);
+                    Sleep(10);
+                    send(current_client,sendData,sizeof(sendData),0);
 
-                strcpy(sendData,parameter);
+                    char bingoLine [100];
+                    // print bingo board line by line
+                    for (int i = 0; i < 7; i++) {
+                        strcpy(sendData, bingoBoard.printDelimiterBoard(bingoLine,i));
+                        while (send(current_client,sendData,sizeof(sendData),0) < 0){
+                            send(current_client,sendData,sizeof(sendData),0);
+                        }
+                        strcpy(bingoLine, "");
+                        strcpy(sendData, "");
+                    }
+
+                } else {
+                    strcpy(message, (char*)"Invalid bingo setup provided. Please try again.");
+                }
+                strcpy(sendData, message);
+                Sleep(10);
+                send(current_client,sendData,sizeof(sendData),0);
+
+            }
+
+            else if(strstr(buf,"markBoard"))
+            { // call markBoard function
+                printf("\nReceived markBoard RPC\n");
+                char* parameter = processParameter(buf);
+                bool success = bingoBoard.markBoard();
+                char message [100];
+                if (success) {
+                    strcpy(message, (char*)"Successfully marked bingo board!");
+                    strcpy(sendData, message);
+                    Sleep(10);
+                    send(current_client,sendData,sizeof(sendData),0);
+
+                    char bingoLine [100];
+                    // print bingo board line by line
+                    for (int i = 0; i < 7; i++) {
+                        strcpy(sendData, bingoBoard.printDelimiterBoard(bingoLine,i));
+                        while (send(current_client,sendData,sizeof(sendData),0) < 0){
+                            send(current_client,sendData,sizeof(sendData),0);
+                        }
+                        strcpy(bingoLine, "");
+                        strcpy(sendData, "");
+                    }
+
+                } else {
+                    strcpy(message, (char*)"Number called by the server not found in current bingo board.");
+                }
+                strcpy(sendData, message);
+                Sleep(10);
+                send(current_client,sendData,sizeof(sendData),0);
+
+            }
+            else if(strstr(buf,"checkBingo"))
+            { // call checkBingo function
+                printf("\nReceived checkBingo RPC\n");
+                bool success = bingoBoard.checkBingo();
+                char message [100];
+                if (success) {
+                    strcpy(message, (char*)"Bingo detected! Game will now end...");
+                    //TODO: MAKE GAME END AND OFFER TO PLAY AGAIN OR DISCONNECT CLIENT
+                } else {
+                    strcpy(message, (char*)"No bingo detected. Please try again later!");
+                }
+                strcpy(sendData, message);
                 Sleep(10);
                 send(current_client,sendData,sizeof(sendData),0);
             }
