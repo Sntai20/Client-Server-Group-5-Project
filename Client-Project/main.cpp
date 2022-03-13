818b6a3cfa111d402b38df0f7b6c1671095ad2f0
Antonio Santana <ansantan@outlook.com>
2022-03-13 16:22:13 -0700

    Disconnect rpc works.

--- Client-Project/main.cpp
+++ Client-Project/main.cpp
@@ -1,84 +1,68 @@
-/**
- * @file main.cpp
- * @author Group 5
- * @brief Client side C++ 17 program to demonstrate Socket programming for Bingo game.
- * @version 0.1
- * @date 2022-02-11
- */  
-#include "client.cpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <iostream>
 
-/**
- * @brief This is the entry for the client program.
- * 
- * @param argc 
- * @param argv 
- * @return int 
- */
-int main(int argc, char* argv[])
+constexpr unsigned int SERVER_PORT = 50544;
+constexpr unsigned int MAX_BUFFER = 128;
+
+int main(int argc, char *argv[])
 {
-    Client myClient;
-    
-    const char* connectRPC = "connect;MIKE;MIKE;";
-    // TODO: const char* statusRPC = "status;";
-    const char* logoffRPC = "disconnect;";
-    char buffer[1024] = { 0 };
-    const char* serverAddress = argv[1];
-
-    /* stoi Function for converting a string to an integer. 
-     * https://www.techiedelight.com/convert-string-to-int-cpp/
-     */
-    const int port = std::stoi(argv[2]);
-    bool bConnect = myClient.ConnectToServer(serverAddress, port);
-
-    if (bConnect)
+    if (argc != 2)
     {
-        std::cout << "connected" << endl;
-        // TODO: strcpy(buffer, connectRPC);
-        // TODO:int nlen = strlen(buffer);
-        // TODO:buffer[nlen] = 0;   // Put the null terminator
-
-        // TODO:send(sock, buffer, strlen(buffer) + 1, 0);
-        // TODO:read(sock, buffer, 1024);
-
-        // TODO:read(sock, buffer, 1024);
-        cout << buffer << endl;
-
-        cout << "What is your message for the server? " << endl;
-        char message[80];
-        std::cin.getline(message, sizeof message);
-        // TODO:send(sock, message, strlen(message) + 1, 0);
-    }
-    else
-    {
-        cout << "\nExit without calling RPC." << endl;
+        std::cerr << "Error! usage: ./client localhost" << std::endl;
+	return 6;
     }
 
-    /**
-     * @brief sleep_for 1 to 10 seconds randomly
-     * https://www.cplusplus.com/reference/thread/this_thread/sleep_for/
-     */ 
-    int seconds = 1 + (rand() % static_cast<int>(10));
-    cout << "\nSleeping..." << endl;
-    std::this_thread::sleep_for(std::chrono::seconds(seconds));
-
-    // Disconnect Message
-    if (bConnect)
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
     {
-        // TODO:strcpy(buffer, logoffRPC);
-        // TODO:int nlen = strlen(buffer);
-        // TODO:buffer[nlen] = 0;   // Put the null terminator
-        // TODO:send(sock, buffer, strlen(buffer) + 1, 0);
-        // TODO: read(sock, buffer, 1024);
-
-        cout << "\nDisconnected." << endl;
-    }
-    else
-    {
-        cout << "\nExit without calling RPC.\n" << endl;
+        std::cerr << "socket error" << std::endl;
+        return 1;
     }
 
-    // Terminate connection.
-    close(myClient.sock);
+    struct hostent* server = gethostbyname(argv[1]);
+    if (server == nullptr) 
+    {
+        std::cerr << "gethostbyname, no such host" << std::endl;
+        return 2;
+    }
 
+    struct sockaddr_in serv_addr;
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+          (char *)&serv_addr.sin_addr.s_addr, 
+          server->h_length);
+    serv_addr.sin_port = htons(SERVER_PORT);
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cerr << "connect error: the server does not look running" << std::endl;
+        return 3;
+    }
+
+    std::string readBuffer (MAX_BUFFER, 0);
+    if (read(sockfd, &readBuffer[0], MAX_BUFFER-1) < 0)
+    {
+        std::cerr << "read from socket failed" << std::endl;
+        return 5;
+    }
+    std::cout << readBuffer << std::endl;
+
+    std::string writeBuffer (MAX_BUFFER, 0);
+    std::cout << "What message for the server? : ";
+    getline(std::cin, writeBuffer);
+    if (write(sockfd, writeBuffer.c_str(), strlen(writeBuffer.c_str())) < 0) 
+    {
+        std::cerr << "write to socket" << std::endl;
+        return 4;
+    }
+
+    close(sockfd);
     return 0;
 }
--- Server-Project/main.cpp
+++ Server-Project/main.cpp
@@ -1,42 +1,125 @@
-/**
- * @file main.cpp
- * @author Group 5
- * @brief Server side C++ program to demonstrate Socket programming for Bingo game.
- * @version 0.1
- * @date 2022-02-11
- */  
-#include "server.cpp"
-/**
- * @brief This is the entry for the server program.
- * 
- * @param argc 
- * @param argv 
- * @return int 
- */
-int main(int argc, char* argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <arpa/inet.h>
+
+
+constexpr unsigned int SERVER_PORT = 50544;
+constexpr unsigned int MAX_BUFFER = 128;
+constexpr unsigned int MSG_REPLY_LENGTH = 18;
+
+bool relationalOperation(std::string s1, std::string s2)
 {
-    char* serverIP = argv[1];
-    int port = atoi(argv[2]);
-    bool statusOk = true;
-
-    Server* myServer = new Server();
-    myServer->SetIPAddress(serverIP);
-    myServer->SetPort(port);
-
-    statusOk = myServer->StartServer(); // Print when server is working
-    cout << "\nServer is up!" << endl;
-
-    while (statusOk)
+ 
+    if (s1 != s2)
     {
-        /**
-         * @brief ListenForClient is used to call the MultiThreadedProcessRPC. 
-         * MultiThreadedProcessRPC starts the threads and to process the client 
-         * connections.
-         */
-        statusOk = myServer->ListenForClient(); 
-        cout << "Server is online listening for client connections!" << endl;
+        std::cout << s1 << " is not equal to " << s2 << std::endl;
+        if (s1 > s2)
+            std::cout << s1 << " is greater than " << s2 << std::endl;
+        else
+            std::cout << s2 << " is greater than " << s1 << std::endl;
+        return false;
+    }
+    else
+        std::cout << s1 << " is equal to " << s2 << std::endl;
+        return true;
+}
+
+char* processParameter(char* buffer) 
+{
+    char* token;
+    printf("input: %s\n", buffer);
+    char *start = buffer;
+    char *end = buffer;
+    while(*buffer) {
+        if(*buffer == '(')
+            start = buffer;
+        else if(*buffer == ')')
+            end = buffer;
+        if(start < end && *start) {
+            *end = 0;
+            token = start + 1;
+            start = buffer = end;
+            break;
+        }
+        buffer++;
+    }
+    return token;
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+     {
+          std::cerr << "open socket error" << std::endl;
+          return 1;
+     }
+
+     int optval = 1;
+     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+
+     struct sockaddr_in serv_addr, cli_addr;
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     serv_addr.sin_family = AF_INET;  
+     serv_addr.sin_addr.s_addr = INADDR_ANY;  
+     serv_addr.sin_port = htons(SERVER_PORT);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+     {
+          std::cerr << "bind error" << std::endl;
+          return 2;
+     }
+
+     listen(sockfd, 5);
+     socklen_t clilen = sizeof(cli_addr);
+     int incomingSock = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     if (incomingSock < 0)
+     {
+          std::cerr << "accept error" << std::endl;
+	     return 3;
+     }
+
+     std::cout << "server: got connection from = " 
+	          << inet_ntoa(cli_addr.sin_addr)
+	          << " and port = " << ntohs(cli_addr.sin_port) << std::endl;
+     write(incomingSock, "You are connected!", MSG_REPLY_LENGTH);
+
+     std::string buffer (MAX_BUFFER, 0);
+     if (read(incomingSock, &buffer[0], MAX_BUFFER-1) < 0)
+     {
+          std::cerr << "read from socket error" << std::endl;
+          return 4;
+     }
+     std::cout << "Got the message:" << buffer << std::endl;
+     // buffer to hold our sent data
+    char sendData[100];
+     
+    std::string rest{buffer};
+    //  if(strstr(buffer,"disconnect"))
+    std::string s1(rest.data());
+    std::string s2("disconnect");
+    
+    if (relationalOperation(s1, s2))
+    { // disconnect this user
+        
+        printf("\nReceived disconnect RPC\n");
+
+        strcpy(sendData,"Disconnected!\n");
+        // Sleep(10);
+        // send(current_client,sendData,sizeof(sendData),0);
+        write(incomingSock, sendData, MSG_REPLY_LENGTH);
+
+        // close the socket associated with this client and end this thread
+        // closesocket(current_client);
+        // ExitThread(0);
     }
 
-    delete myServer;
-    return 0;
+     close(incomingSock);
+     close(sockfd);
+     return 0; 
 }
