ffffd208151aaed56208e89b1940b305bbb650b2
Antonio Santana <ansantan@outlook.com>
2022-03-13 15:45:22 -0700

    Just notes.

--- Server-Project/wsattemp1.cpp
+++ Server-Project/wsattemp1.cpp
@@ -270,82 +270,84 @@ char* processParameter(char* buf)
 }
 */
 
-#define PORT 8080
-
 int main()
 {
     printf("Starting up multi-threaded TCP server!\r\n");
+
+    // our masterSocket(socket that listens for connections)
+    // SOCKET sock;
+    unsigned long SocketFileDescriptor;
 
     // for our thread
     // DWORD thread;
     int thread;
     
-    int server_fd, new_socket, valread;
+    // The WSADATA structure contains information about the Windows Sockets implementation.
+    // WSADATA wsaData; 
+    // sockaddr_in address;
     struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-       
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-       
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    // int socket(int domain, int type, int protocol);
+
+    // start winsock Not needed for linux
+    // int ret = WSAStartup(0x101,&wsaData); // use highest version of winsock avalible
+
+    // if(ret != 0)
+    // {
+    //     return 0;
+    // }
+
+    // fill in winsock struct ...
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-       
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
+    address.sin_port = htons(8080); // listen on telnet port 8080
+
+    // create our socket
+    SocketFileDescriptor = socket(AF_INET,SOCK_STREAM,0);
+    const int INVALID_SOCKET = -1;
+    if(SocketFileDescriptor == INVALID_SOCKET)
     {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+        return 0;
     }
 
-    if (listen(server_fd, 3) < 0)
+    // bind our socket to a port(port 8080)
+    // bind(SocketFileDescriptor,
+    // (struct sockaddr*)&server,sizeof(server)
+    if(bind(SocketFileDescriptor,(struct sockaddr *)&address,
+                                    sizeof(address))<0)
     {
-        perror("listen");
-        exit(EXIT_FAILURE);
+        return 0;
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
+
+    // listen for a connection
+    if(listen(SocketFileDescriptor,5) != 0)
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
+        return 0;
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    // socket that we snedzrecv data on
+    // SOCKET client;
+    int client;
+
+    sockaddr_in from;
+    int fromlen = sizeof(from);
 
     // loop forever
-    // while (true)
-    // {
-    //     // accept connections
-    //     // client = accept(SocketFileDescriptor,(struct sockaddr*)&from,&fromlen);
-    //     client = accept(SocketFileDescriptor, (struct sockaddr*)&server, (socklen_t*)&fromlen);
-    //     printf("Client connected!\r\n");
+    while (true)
+    {
+        // accept connections
+        // client = accept(SocketFileDescriptor,(struct sockaddr*)&from,&fromlen);
+        client = accept(SocketFileDescriptor, (struct sockaddr*)&server, (socklen_t*)&fromlen);
+        printf("Client connected!\r\n");
 
-    //     // create our recv_cmds thread and parse client socket as a parameter
-    //     // CreateThread(NULL, 0,receive_cmds,(void*)client, 0, &thread);
-    //     std::thread t1 (NULL, 0,receive_cmds,client, 0, &thread);
+        // create our recv_cmds thread and parse client socket as a parameter
+        // CreateThread(NULL, 0,receive_cmds,(void*)client, 0, &thread);
+        std::thread t1 (NULL, 0,receive_cmds,client, 0, &thread);
         
-    // }
+    }
 
     // shutdown winsock
     // closesocket(sock);
-    // close(SocketFileDescriptor);
+    close(SocketFileDescriptor);
     // WSACleanup();
 
     // exit
